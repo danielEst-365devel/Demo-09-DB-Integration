3cbc08032fd2c66a6bd4f91360a9b02faf19ab8d
Daniel Est <daniel.anth.est.03@gmail.com>
2024-11-26 12:27:22 +0800

    a

--- api/index.js
+++ api/index.js
@@ -29,7 +29,7 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, {
 	customCss:
 		'.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }',
 	customCssUrl: CSS_URL,
-}));F
+}));
 
 // http://localhost:4000/tasks
 app.get('/tasks', async (req, res) => {
